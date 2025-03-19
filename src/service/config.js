b4a5fb771da73f694e6f6c11543cee3e5b401181
Vikas Vaibhav <sinhaamanas2832439@gmail.com>
2025-03-19 16:24:45 +0530

    chore: update config for local development URLs

--- src/service/config.js
+++ src/service/config.js
@@ -6,5 +6,9 @@ import {Platform} from 'react-native';
 //   Platform.OS === 'android' ? 'ws://10.0.2.2:3000' : 'ws:/localhost:3000';
 
 // USE YOUṘ NETWORK IP OR HOSTED URL
-// export const BASE_URL = 'https://meet-server-gctk.onrender.com';
-// export const SOCKET_URL = 'wss://meet-server-gctk.onrender.com';
+//  export const BASE_URL = 'https://meet-server-gctk.onrender.com';
+//  export const SOCKET_URL = 'wss://meet-server-gctk.onrender.com';
+
+// USE YOUR LOCAL IP ADDRESS
+export const BASE_URL = 'http://192.168.202.170:3000';
+export const SOCKET_URL = 'ws://192.168.202.170:3000';
